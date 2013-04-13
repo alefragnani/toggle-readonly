21edb241d9e56c8cf71380514eea9da754c3ded4
Alessandro Fragnani <alefragnani@hotmail.com>
2013-04-13 10:17:56 -0300

    First update  - Created Command  - Removed .pyc from repo  - Removed package-metadata.json from repo  - Updated the code to handle ON/OFF read-only attr and also becoming compatible with Mac OS X

--- toggle_readonly.py
+++ toggle_readonly.py
@@ -4,16 +4,13 @@ class ToggleReadonlyCommand(sublime_plugin.TextCommand):
     def run(self, edit):
       myFile = self.view.file_name()
       fileAtt = os.stat(myFile)[0]
-      myPlatform = os.name
 
-      if (myPlatform == 'nt'):
-        if (not fileAtt & stat.S_IWRITE):
-          sublime.status_message("Making "+myFile+" writable")
-          os.chmod(myFile, stat.S_IWRITE)
+      if (not fileAtt & stat.S_IWRITE):
+        sublime.status_message("Making "+myFile+" writable")
+        os.chmod(myFile, stat.S_IWRITE)
       else:
-        if (fileAtt & stat.UF_IMMUTABLE):
-          sublime.status_message("Making "+myFile+" mutable")
-          os.chflags(myFile, not stat.UF_IMMUTABLE)
+        sublime.status_message("Making "+myFile+" not writable")
+        os.chmod(myFile, stat.S_IREAD)          
 
     def is_enabled(self):
         return self.view.file_name() and len(self.view.file_name()) > 0
